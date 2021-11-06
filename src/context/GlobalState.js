59a7f6c3eb61d0c42dae3fe637d51cc57d4d6795
Stackbit Code Editor <projects@stackbit.com>
2021-11-06 23:00:39 +0000

    GlobalState.js: changed

---
+++ src/context/GlobalState.js
@@ -0,0 +1,27 @@
+import React, { createContext, useReducer } from 'react';
+import AppReducer from './AppReducer';
+
+// Initial state
+const initialState = {
+    category: 'View all'
+}
+
+// Create context
+export const GlobalContext = createContext(initialState);
+
+// Provider component
+export const GlobalProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(AppReducer, initialState);
+
+    // Actions
+    function setCategory(category) {
+        dispatch({
+            type: 'SET_CATEGORY',
+            payload: category
+        })
+    }
+
+    return (
+        <GlobalContext.Provider value={{ category: state.category, setCategory }}>{children}</GlobalContext.Provider>
+    )
+}
