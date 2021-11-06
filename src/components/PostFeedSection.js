a55b653381be8438da97fd7bd8726035da4d7ebe
Stackbit Code Editor <projects@stackbit.com>
2021-11-06 23:33:54 +0000

    PostFeedSection.js: changed

--- src/components/PostFeedSection.js
+++ src/components/PostFeedSection.js
@@ -135,7 +135,7 @@ function postsVariantA(props) {
                                 <time dateTime={dateTimeAttr} data-sb-field-path="date">
                                     {formattedDate}
                                 </time>
-                                
+                                <span></span>
                             </div>
                             {post.excerpt && <p data-sb-field-path="excerpt">{post.excerpt}</p>}
                         </div>
