609ef5e9eb7f459cedac5c10e72fedc672c172e2
Stackbit Code Editor <projects@stackbit.com>
2021-11-06 22:40:34 +0000

    FilterButton.js: changed

--- src/components/FilterButton.js
+++ src/components/FilterButton.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import classNames from 'classnames';
 import Link from '@stackbit/components/dist/utils/link';
-import ArrowLeft from '@s';
+import ArrowLeft from '@stackbit/components/dist/';
 import ArrowRight from '../../svgs/arrow-right';
 import Cart from '../../svgs/cart';
 import Facebook from '../../svgs/facebook';
