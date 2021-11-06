abf76698d270755c0b97fb13bfd98d4f5ef058c8
Stackbit Code Editor <projects@stackbit.com>
2021-11-06 23:02:00 +0000

    Action.js: changed

--- src/components/Action.js
+++ src/components/Action.js
@@ -42,7 +42,7 @@ export default function Action(props) {
     const style = props.style || defaultStyle;
     const cssClasses = props.className || null;
     const cssId = props.elementId || null;
-    
+
     const { category, setCategory } = useContext(GlobalContext);
 
     console.log(category);
