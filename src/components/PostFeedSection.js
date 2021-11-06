26b96db918e51264dffae345ce70dd3e955d1b4e
Stackbit Code Editor <projects@stackbit.com>
2021-11-06 23:20:59 +0000

    PostFeedSection.js: changed

--- src/components/PostFeedSection.js
+++ src/components/PostFeedSection.js
@@ -78,7 +78,7 @@ function PostFeedActions(props) {
     const Action = getComponent('Action');
     return (
         <div
-            className={classNames('flex', 'flex-wrap', 'items-center', '-mx-2', 'mb-5', styles.actions ? mapStyles(styles.actions) : null)}
+            className={classNames('flex', 'flex-wrap', 'items-center', '-mx-2', 'mb-6', styles.actions ? mapStyles(styles.actions) : null)}
             data-sb-field-path=".actions"
         >
             {props.actions.map((action, index) => (
