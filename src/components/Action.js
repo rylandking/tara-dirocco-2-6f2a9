53af6cc9cd1d39958a78401e1a1afca3815e166b
Stackbit Code Editor <projects@stackbit.com>
2021-11-06 22:46:07 +0000

    Action.js: changed

--- src/components/Action.js
+++ src/components/Action.js
@@ -39,7 +39,7 @@ export default function Action(props) {
     const style = props.style || defaultStyle;
     const cssClasses = props.className || null;
     const cssId = props.elementId || null;
-    return <p>
+
     return (
         <Link
             href={url}
