3ee3b876331f2d20e0ff1562316f98f6460a0f3b
Stackbit Code Editor <projects@stackbit.com>
2021-11-07 21:16:18 +0000

    VideoEmbed.js: changed

--- src/components/VideoEmbed.js
+++ src/components/VideoEmbed.js
@@ -1,9 +1,9 @@
-const VideoEmbed = ({ annotationPrefix, videoUrl, videoTitle }) => {
+const VideoEmbed = ({ annotationPrefix, videoId, videoTitle }) => {
     console.log(videoUrl);
 
     return (
         <div className="flex justify-center my-5" data-sb-field-path={annotationPrefix}>
-            <iframe width="560" height="315" src={videoUrl} title={videoTitle} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe width="560" height="315" src={videoId} title={videoTitle} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
     )
 }
