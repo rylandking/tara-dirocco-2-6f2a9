7fb90e1f956e2c3fbb31572ecd0a8f73d168be9b
Stackbit Code Editor <projects@stackbit.com>
2021-11-07 23:18:43 +0000

    FeaturedCardsSection.js: changed

--- src/components/FeaturedCardsSection.js
+++ src/components/FeaturedCardsSection.js
@@ -40,6 +40,21 @@ export default function FeaturedCardsSection({
                             <p data-sb-field-path="excerptOne">{excerptOne}</p>
                         </div>
                     </article>
+                    {/* Feature Card One */}
+                    <article className="sb-card">
+                        <Link href={linkOne} className="block h-0 w-full pt-9/16 relative" data-sb-field-path="featuredImageOne">
+                            <ImageBlock {...featuredImageOne} className="absolute left-0 top-0 h-full w-full object-cover" />
+                            {/* <ImageBlock {...post.featuredImage} className="absolute left-0 top-0 h-full w-full object-cover" /> */}
+                        </Link>
+                        <div className="px-4 py-6 sm:px-6 sm:pb-10">
+                            <h2 className="text-xl sm:text-2xl mb-1">
+                                <Link href={linkOne} data-sb-field-path="titleOne">
+                                    {titleOne}
+                                </Link>
+                            </h2>
+                            <p data-sb-field-path="excerptOne">{excerptOne}</p>
+                        </div>
+                    </article>
                 </div>
             </div>
         </div>
