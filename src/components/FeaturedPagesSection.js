8db52250e973b94ca7395d920bf97c4c669e8646
Stackbit Code Editor <projects@stackbit.com>
2021-11-07 22:44:09 +0000

    FeaturedPagesSection.js: changed

--- src/components/FeaturedPagesSection.js
+++ src/components/FeaturedPagesSection.js
@@ -8,7 +8,7 @@ export default function FeaturedPagesSection({ annotationPrefix, title, subtitle
             data-sb-field-path={annotationPrefix}
         >
             <div className="flex flex-col max-w-screen-2xl">
-                <div className="w-full mx-auto">
+                <div className="w-full justify-center">
                     <h2 className="text-3xl sm:text-4xl">{title}</h2>
                     <p className="text-lg sm:text-xl">{subtitle}</p>
                     {/* {featuredPages(props)} */}
