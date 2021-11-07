9205db0cce51c357991b857ac260b0328ed10627
Stackbit Code Editor <projects@stackbit.com>
2021-11-07 21:48:01 +0000

    PodcastSection.js: changed

--- src/components/PodcastSection.js
+++ src/components/PodcastSection.js
@@ -1,4 +1,4 @@
-const PodcastSection = ({ annotationPrefix, podcastLink, podcastTitle, podcast }) => {
+const PodcastSection = ({ annotationPrefix, podcastLink, podcastTitle }) => {
 
     return (
         <div className="w-full sm:w-11/12 lg:w-3/4 mx-auto mt-5 mb-10">
