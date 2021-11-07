c68b23cd8d5e37e7fbbf65b201f94dbbb0d33bf7
Stackbit Code Editor <projects@stackbit.com>
2021-11-07 00:10:11 +0000

    Action.js: changed

--- src/components/Action.js
+++ src/components/Action.js
@@ -49,7 +49,7 @@ export default function Action(props) {
 
     let canSetCategory = false;
 
-    if (label === 'Blog' || label === 'Podcast' || label === 'Video' || label === 'View all') {
+    if (label === 'Blog' || label === 'Podcast' || label === 'Video') {
         canSetCategory = true;
     }
 
