3a711b076289e1d627c2f55416331d3cf08d61f5
Stackbit Code Editor <projects@stackbit.com>
2021-11-07 21:13:43 +0000

    VideoEmbed.js: changed

--- src/components/VideoEmbed.js
+++ src/components/VideoEmbed.js
@@ -2,7 +2,7 @@ const VideoEmbed = ({ videoUrl, videoTitle }) => {
     console.log(videoUrl);
 
     return (
-        <div className="flex justify-center">
+        <div className="flex justify-center my-5">
             <iframe width="560" height="315" src={videoUrl} title={videoTitle} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
     )
