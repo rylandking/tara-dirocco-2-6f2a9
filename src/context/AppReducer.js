3678318e6f688bd152a9aed1c88379b4aa723c8e
rylandking <ryland@stackbit.com>
2021-11-07 12:47:00 -0700

    define reducer

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,11 +1,13 @@
-export default (state, action) => {
+const reducer = (state, action) => {
     switch (action.type) {
         case 'SET_CATEGORY':
             return {
                 ...state,
                 category: action.payload
-            }
+            };
         default:
             return state;
     }
-}
+};
+
+export default reducer;
