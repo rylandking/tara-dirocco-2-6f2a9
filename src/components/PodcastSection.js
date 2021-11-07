c27e8798680a2a3817323098cafc29203de04347
Stackbit Code Editor <projects@stackbit.com>
2021-11-07 21:49:20 +0000

    PodcastSection.js: changed

--- src/components/PodcastSection.js
+++ src/components/PodcastSection.js
@@ -2,9 +2,7 @@ const PodcastSection = ({ annotationPrefix, podcastLink, podcastTitle }) => {
 
     return (
         <div className="w-full sm:w-11/12 lg:w-3/4 mx-auto mt-5 mb-10">
-            <div className="w-full h-0 relative" style={{ paddingBottom: '56.25%' }} data-sb-field-path={annotationPrefix}>
-                <iframe className="absolute top-0 left-0 w-full h-full" src={`https://www.youtube-nocookie.com/embed/${videoId}`} title={videoTitle} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            </div>
+            <p>123</p>
         </div>
     )
 }
