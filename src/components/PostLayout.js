8cbfd4c1cc901cb4fd18625e3d62a460ae63ccd1
Stackbit Code Editor <projects@stackbit.com>
2021-11-07 21:40:18 +0000

    PostLayout.js: changed

--- src/components/PostLayout.js
+++ src/components/PostLayout.js
@@ -31,11 +31,11 @@ export default function PostLayout(props) {
                                 {page.author && postAuthor(page.author)}
                             </div>
                         </header>
-                        {page.featuredImage && (
+                        {/* {page.featuredImage && (
                             <figure className="h-0 w-full pt-9/16 max-w-screen-xl mx-auto mb-8 relative" data-sb-field-path="featuredImage">
                                 <ImageBlock {...page.featuredImage} className="absolute left-0 top-0 h-full w-full object-cover" />
                             </figure>
-                        )}
+                        )} */}
                         {topSections.length > 0 && (
                             <div data-sb-field-path="topSections">
                                 {topSections.map((section, index) => {
