9fbd76b6eef04853b28c670ca490232b2dd4af9f
Stackbit Code Editor <projects@stackbit.com>
2021-11-07 23:08:07 +0000

    FeaturedCardsSection.js: changed

--- src/components/FeaturedCardsSection.js
+++ src/components/FeaturedCardsSection.js
@@ -20,7 +20,7 @@ export default function FeaturedCardsSection({ annotationPrefix, title, subtitle
                             </Link>
                         <div className="px-4 py-6 sm:px-6 sm:pb-10">
                             <h2 className="text-xl sm:text-2xl mb-1">
-                                <Link href={getPageUrlPath(post)} data-sb-field-path="title">
+                                <Link href="" data-sb-field-path="title">
                                     {post.title}
                                 </Link>
                             </h2>
