a8832eb628b7b522061908272bdc8c2ea6c45491
Stackbit Code Editor <projects@stackbit.com>
2021-11-07 23:08:57 +0000

    FeaturedCardsSection.js: changed

--- src/components/FeaturedCardsSection.js
+++ src/components/FeaturedCardsSection.js
@@ -1,7 +1,8 @@
 import * as React from 'react';
 
 export default function FeaturedCardsSection({ 
-    annotationPrefix, title, subtitle }) {
+    annotationPrefix, title, subtitle 
+}) {
 
     return (
         <div
