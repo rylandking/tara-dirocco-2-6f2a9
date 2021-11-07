3d65bdcdb0c7a2a6b1c7c77150065b5046fe58b5
Stackbit Code Editor <projects@stackbit.com>
2021-11-07 21:07:04 +0000

    VideoEmbed.js: changed

--- src/components/VideoEmbed.js
+++ src/components/VideoEmbed.js
@@ -1,4 +1,4 @@
-const VideoEmbed = ({videoURL}) => {
+const VideoEmbed = () => {
     return (
         <div>
             <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/bN__rPgZqCw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
