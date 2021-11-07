fdfbbb3ed81767d8b00e45b3800eef3240f71ea1
Stackbit Code Editor <projects@stackbit.com>
2021-11-07 23:02:58 +0000

    FeaturedCardsSection.js: changed

--- src/components/FeaturedCardsSection.js
+++ src/components/FeaturedCardsSection.js
@@ -14,7 +14,6 @@ export default function FeaturedCardsSection({ annotationPrefix, title, subtitle
                 </div>
                 <div className="grid gap-6 md:grid-cols-3 lg:gap-8">
                     <article className="sb-card">
-                        
                             <Link href={getPageUrlPath(post)} className="block h-0 w-full pt-9/16 relative" data-sb-field-path="featuredImage">
                                 <ImageBlock {...post.featuredImage} className="absolute left-0 top-0 h-full w-full object-cover" />
                             </Link>
