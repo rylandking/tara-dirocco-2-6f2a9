cc559702470435db5397cdeb15cac9a3da74cf01
Stackbit Code Editor <projects@stackbit.com>
2021-11-07 23:39:46 +0000

    FeaturedCardsSection.js: changed

--- src/components/FeaturedCardsSection.js
+++ src/components/FeaturedCardsSection.js
@@ -50,17 +50,17 @@ export default function FeaturedCardsSection({
                     </Link>
                     {/* Feature Card Two */}
                     <Link href={linkTwo}>
-                    <article className="sb-card">
-                        <div className="block h-0 w-full pt-9/16 relative" data-sb-field-path="featuredImageTwo">
-                            <ImageBlock {...featuredImageTwo} className="absolute left-0 top-0 h-full w-full object-cover" />
-                        </Link>
-                        <div className="px-4 py-6 sm:px-6 sm:pb-10 bg-gray-100">
-                            <h2 className="text-xl sm:text-2xl mb-1" data-sb-field-path=".titleTwo">
-                                {titleTwo}
-                            </h2>
-                            <p data-sb-field-path=".excerptTwo">{excerptTwo}</p>
-                        </div>
-                    </article>
+                        <article className="sb-card">
+                            <div className="block h-0 w-full pt-9/16 relative" data-sb-field-path="featuredImageTwo">
+                                <ImageBlock {...featuredImageTwo} className="absolute left-0 top-0 h-full w-full object-cover" />
+                            </div>
+                            <div className="px-4 py-6 sm:px-6 sm:pb-10 bg-gray-100">
+                                <h2 className="text-xl sm:text-2xl mb-1" data-sb-field-path=".titleTwo">
+                                    {titleTwo}
+                                </h2>
+                                <p data-sb-field-path=".excerptTwo">{excerptTwo}</p>
+                            </div>
+                        </article>
                     </Link>
                     {/* Feature Card Three */}
                     <article className="sb-card">
