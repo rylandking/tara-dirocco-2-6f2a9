bd6829382e5b579fb840f1a67ac086aff175b3b4
Stackbit Code Editor <projects@stackbit.com>
2021-11-07 22:24:01 +0000

    register-components.js: changed

--- src/components/register-components.js
+++ src/components/register-components.js
@@ -9,7 +9,7 @@ registerComponents({
     PostFeedSection: dynamic(() => import('./PostFeedSection.js')),
     PostLayout: dynamic(() => import('./PostLayout.js')),
     VideoEmbed: dynamic(() => import('./VideoEmbed.js')),
-    FeaturedPagesSection: dynamic(() => import('./FeaturePagesSection.js'))
+    FeaturedPagesSection: dynamic(() => import('./FeaturedPagesSection.js'))
 
     // Override any static or dynamic component,
     // or register your own dynamic component.
