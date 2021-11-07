085925085bf6c5fac4072e1dc360a68ca7321577
Stackbit Code Editor <projects@stackbit.com>
2021-11-07 22:35:27 +0000

    FeaturedPagesSection.js: changed

--- src/components/FeaturedPagesSection.js
+++ src/components/FeaturedPagesSection.js
@@ -62,14 +62,7 @@ export default function FeaturedPagesSection(props) {
     return (
         <div
             id={props.elementId}
-            className="sb-component sb-component-section',
-                backgroundWidth === 'inset' ? 'sb-component-section-inset' : null,
-                'sb-component-featured-posts-section',
-                colors,
-                'px-4',
-                'sm:px-8',
-                sectionStyles.margin
-            )}
+            className=""
             data-sb-field-path={props.annotationPrefix}
         >
             <div
