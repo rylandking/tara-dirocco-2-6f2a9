39eedbb0f6a775f87423a332e2e31607b53c01a9
Stackbit Code Editor <projects@stackbit.com>
2021-11-07 21:21:55 +0000

    VideoEmbed.js: changed

--- src/components/VideoEmbed.js
+++ src/components/VideoEmbed.js
@@ -2,7 +2,7 @@ const VideoEmbed = ({ annotationPrefix, videoId, videoTitle }) => {
 
     return (
         <div className="flex justify-center my-5 w-full relative" data-sb-field-path={annotationPrefix}>
-            <iframe width="560" height="315" src={`https://www.youtube-nocookie.com/embed/${videoId}`} title={videoTitle} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe className="absolute w-full h-full" src={`https://www.youtube-nocookie.com/embed/${videoId}`} title={videoTitle} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
     )
 }
