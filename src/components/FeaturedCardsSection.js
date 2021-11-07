a8a7281f4562d88ed474a5472634e6933e268ce4
Stackbit Code Editor <projects@stackbit.com>
2021-11-07 23:33:17 +0000

    FeaturedCardsSection.js: changed

--- src/components/FeaturedCardsSection.js
+++ src/components/FeaturedCardsSection.js
@@ -24,7 +24,7 @@ export default function FeaturedCardsSection({
 
     return (
         <div
-            className="w-full flex justify-center pt-6 pb-10 px-8 md:px-44 xl:px-32 bg-yellow-100"
+            className="w-full flex justify-center pt-6 pb-10 px-8 xl:px-32 bg-yellow-100"
             data-sb-field-path={annotationPrefix}
         >
             <div className="flex flex-col max-w-screen-2xl">
