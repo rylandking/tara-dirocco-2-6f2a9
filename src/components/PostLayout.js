d960624d8aedb2a36be7ccad7c37fb9e9e93544b
Stackbit Code Editor <projects@stackbit.com>
2021-11-07 00:33:09 +0000

    PostLayout.js: changed

--- src/components/PostLayout.js
+++ src/components/PostLayout.js
@@ -8,8 +8,8 @@ import { getComponent } from '@stackbit/components/dist/components-registry';
 export default function PostLayout(props) {
     const { page, site } = props;
     const BaseLayout = getBaseLayoutComponent(page.baseLayout, site.baseLayout);
-    const bottomSections = page.bottomSections || [];
     const topSections = page.topSections || [];
+    const bottomSections = page.bottomSections || [];
     const dateTimeAttr = dayjs(page.date).format('YYYY-MM-DD HH:mm:ss');
     const formattedDate = dayjs(page.date).format('MMMM D, YYYY');
 
