ea85e5f9e5f0e5d1176c1ce99471c40cc2290e6a
Stackbit Code Editor <projects@stackbit.com>
2021-11-07 22:34:02 +0000

    FeaturedPagesSection.js: changed

--- src/components/FeaturedPagesSection.js
+++ src/components/FeaturedPagesSection.js
@@ -57,7 +57,7 @@ const posts = [
     },
 ]
 
-export default function FeaturedPostsSection(props) {
+export default function FeaturedPagesSection(props) {
     const colors = props.colors || 'colors-a';
     const backgroundWidth = props.backgroundWidth || 'full';
     const sectionStyles = props.styles?.self || {};
