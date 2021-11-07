6afd9ca597bc29781649982b2bb83663b8c0975b
Stackbit Code Editor <projects@stackbit.com>
2021-11-07 20:59:35 +0000

    VideoEmbed.js: changed

--- src/components/VideoEmbed.js
+++ src/components/VideoEmbed.js
@@ -1,7 +1,8 @@
 const VideoEmbed = () => {
     return (
         <div>
-        <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/bN__rPgZqCw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/bN__rPgZqCw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
     )
 }
 
