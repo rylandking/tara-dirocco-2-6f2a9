f0d1cc4c1e6bc55acf6d5272a5825549b77aa612
Stackbit Code Editor <projects@stackbit.com>
2021-11-07 22:30:54 +0000

    FeaturedPagesSection.js: changed

--- src/components/FeaturedPagesSection.js
+++ src/components/FeaturedPagesSection.js
@@ -57,7 +57,7 @@ const posts = [
     },
 ]
 
-export default function FeaturedPagesSection({ }) {
+export default function FeaturedPagesSection({ title, subtitle }) {
 
     return (
         <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
