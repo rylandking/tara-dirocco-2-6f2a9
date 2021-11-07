4de6bd3194589aa81621b5a3fef73b38cd5af405
Stackbit Code Editor <projects@stackbit.com>
2021-11-07 22:20:58 +0000

    FeaturedPagesSection.js: changed

--- src/components/FeaturedPagesSection.js
+++ src/components/FeaturedPagesSection.js
@@ -1,11 +1,11 @@
 import * as React from 'react';
 
-export default function FeaturedPagesSection(props) {
+export default function FeaturedPagesSection({}) {
 
     return (
         <div
             className=""
-            data-sb-field-path={props.annotationPrefix}
+            // data-sb-field-path={props.annotationPrefix}
         >
             <p>123</p>
         </div>
